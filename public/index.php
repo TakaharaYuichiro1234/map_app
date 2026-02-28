a107bacb4c9d0e672852c130b2349b1ee0226143
Yuichiro Takahara <yuichiro.takahara1234@gmail.com>
2026-02-28 11:10:59 +0000

    bug fix

--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php
 
 session_name('MAP_APP_SESSID');
+session_set_cookie_params([
+    'secure' => true,      // 本番HTTPSで有効
+    'httponly' => true,
+    'samesite' => 'Lax'
+]);
 session_start();
 define('BASE_PATH', '/map_app');
 require_once __DIR__ . '/../vendor/autoload.php';
